3175c6adb641fc3b2b50203b83e168c60fdd1385
NateMachoka <natemachoka@gmail.com>
2024-09-30 12:33:44 -0700

    fixed 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,3 @@
 export default function createIteratorObject(report) {
-  const allEmployees = [];
-
-  // Extract all employees from each department and push them to the allEmployees array
-  for (const department in report.allEmployees) {
-    allEmployees.push(...report.allEmployees[department]);
-  }
-
-  // Return an iterator over the employees array
-  return allEmployees[Symbol.iterator]();
+  return Object.values(report.allEmployees).flat(Infinity)[Symbol.iterator]();
 }
