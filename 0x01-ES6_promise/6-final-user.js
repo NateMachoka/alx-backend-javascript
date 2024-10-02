d4cb4e6f04c0705b17cdb358f099bd22601a1312
NateMachoka <natemachoka@gmail.com>
2024-10-02 10:56:28 -0700

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,17 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const results = await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
+  const promises = [
+    signUpUser(firstName, lastName).then(
+      (value) => ({ status: 'fulfilled', value }),
+      (error) => ({ status: 'rejected', value: error.toString() })
+    ),
+    uploadPhoto(fileName).then(
+      (value) => ({ status: 'fulfilled', value }),
+      (error) => ({ status: 'rejected', value: error.toString() })
+    )
+  ];
 
-  return results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  }));
+  return Promise.all(promises);
 }
