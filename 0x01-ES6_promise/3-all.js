587f721fdf4d54f3cc4165be10d64edb61895a58
NateMachoka <natemachoka@gmail.com>
2024-10-02 03:21:39 -0700

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils.js';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      const [photoResponse, userResponse] = values;
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+      const [photo, user] = values;
+      console.log(`${photo.body} ${userResponse.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
