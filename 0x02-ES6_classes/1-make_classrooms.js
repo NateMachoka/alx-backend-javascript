c1d6d6c07d40899228380e848bdf7fecd0782d25
NateMachoka <natemachoka@gmail.com>
2024-10-03 07:01:58 -0700

    use loops to initialize rooms dynamically

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,6 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+  const sizes = [19, 20, 34];
+  return sizes.map(size => new ClassRoom(size));
 }
