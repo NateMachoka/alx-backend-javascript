d76b451648b62fb34346c9cb46ab0de84f84fd14
NateMachoka <natemachoka@gmail.com>
2024-11-26 04:55:10 -0800

    3-read_file_async.js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs').promises;
+
+/**
+ * Counts the number of students asynchronously
+ * @param {string} path File path
+*/
+
+async function countStudents(filePath) {
+    try {
+	const data = await fs.readFile(filePath, 'utf8');
+	const lines = data.split('\n').filter(line => line.trim !== '');
+	if (lines.length < 2) {
+	    throw new Error('Cannot load the database');
+	}
+	const header = lines.shift();
+	const students = lines.map(line => line.split(',')).filter(fields => fields.length === 4);
+	console.log(`Number of students: ${students.length}`);
+	const fieldMap = {};
+	students.forEach(student => {
+	    const field = student[3];
+	    const firstName = student[0];
+	    if (!fieldMap[field]) {
+		fieldMap[field] = [];
+	    }
+	    fieldMap[field].push(firstName);
+	});
+	for (const [field, names] of Object.entries(fieldMap)) {
+	    console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+	}
+    } catch (error) {
+	throw new Error('Cannot load the database');
+    }
+}
+module.exports = countStudents;
