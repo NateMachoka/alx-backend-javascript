0090e62a72a40ab454d57b01a903615efec98afb
NateMachoka <natemachoka@gmail.com>
2024-11-26 05:37:36 -0800

    5-http.js

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+
+const app = http.createServer(async (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/') {
+    // Root path: Display a welcome message
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (parsedUrl.pathname === '/students') {
+    // Students path: Display student information
+    const databasePath = process.argv[2];
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+
+    if (!databasePath) {
+      res.end('Cannot load the database\n');
+      return;
+    }
+
+    try {
+      await countStudents(databasePath);
+    } catch (error) {
+      res.end(`${error.message}\n`);
+      return;
+    }
+
+    try {
+      const result = await countStudents(databasePath);
+      res.end(result);
+    } catch (error) {
+	      res.end(`${error.message}\n`);
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+app.listen(1245);
+
+module.exports = app;
