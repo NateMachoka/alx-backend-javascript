d04ff91e446e741541cba2103fe28f948be356cd
NateMachoka <natemachoka@gmail.com>
2024-11-27 02:53:24 -0800

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
+// Create the server
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
+
 app.listen(1245);
 
-module.export = app;
+module.exports = app;
